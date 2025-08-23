3e4f4390a65a9889ca805511b6eb4a57957167da
Callum-OP <callumoprey@gmail.com>
2025-08-23 16:38:30 +0100

    Set up a basic plan for implementing the algorithm

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
+// A Star Pathfinding basics
+// G Cost - Travel distance from a node to the start node
+// H Cost - Travel distance from a node to the target node
+// F Cost - G Cost + H Cost
+// The lower the F Cost the better/quicker the path is
+
+// Chose C++ and SFML as I am familiar with the basics of C++ and SFML and displaying objects
+// Chose A Star as it is general purpose and good at finding the quickest path.
+
+// Plan
+// Set up C++ and SFML project
+// Learn Pathfinding
+// Create nodes and a grid of nodes
+// Objects/nodes will include Empty, Obstacles, Path, Start and End points
+// Implement pathfinding from the start to end points
+// Visualise the algorithm working, move an object from start to end
+// Bonus features: Add custom grid building so the user can place their own obstacles or make a specific map
+
+// Things to consider
+// Size of object, will it fit through a one node wide path or will it need a bigger path?
+
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
     sf::CircleShape shape(50);
