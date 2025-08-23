cf40d5d171b380b1222543d27eb9a3348a93f510
Callum-OP <callumoprey@gmail.com>
2025-08-23 16:05:06 +0100

    Setting up SFML installation

---
+++ README.md
@@ -0,0 +1,7 @@
+Will try to make a pathfinding algorithm using C++ and SFML.
+Included SFML files in the project.
+
+To run it simply open main.exe
+
+To compile it again on Windows, assuming you have mingw64 installed you can compile it in Powershell with: 
+g++ -Wall -Wextra -g3 main.cpp -o main.exe -I"C:/SFML/include" -L"C:/SFML/lib" -lsfml-graphics -lsfml-window -lsfml-system
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+    sf::CircleShape shape(50);
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition(375, 275);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
