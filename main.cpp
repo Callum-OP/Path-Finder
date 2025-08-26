796fbe0dc8c810a66fbcba572dac7a2fd35ff8f3
Callum-OP <callumoprey@gmail.com>
2025-08-26 01:40:08 +0100

    Added grid, with option for wall tiles

--- main.cpp
+++ main.cpp
@@ -22,23 +22,63 @@
 // Things to consider
 // Size of object, will it fit through a one node wide path or will it need a bigger path?
 
+// Node structure, can be wall or empty
+struct Node {
+    bool wall = false;
+};
+
+// Grid structure, made up of nodes
+struct Grid {
+    int width, height;
+    std::vector<std::vector<Node>> nodes; // nodes[y][x]
+
+    explicit Grid(int w, int h) : width(w), height(h), nodes(h, std::vector<Node>(w)) {}
+
+    bool inBounds(int x, int y) const {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
+};
+
 int main() {
+    // Create grid
+    const int TILE = 32;
+    const int W = 20;  // columns
+    const int H = 20;  // rows
+    Grid grid(W, H);
+
+    // Create window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
-    sf::CircleShape shape(50);
-    shape.setFillColor(sf::Color::Green);
-    shape.setPosition(375, 275);
+
+    // Create wall
+    grid.nodes[2][2].wall = !grid.nodes[2][2].wall;
 
     while (window.isOpen()) {
         sf::Event event;
+        // Ensure window is closed properly
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        // Clear screen
+        window.clear(sf::Color::Black);
+
+        // Fill in grid
+        sf::RectangleShape tileShape(sf::Vector2f(TILE - 1, TILE - 1));
+        for (float y = 0; y < H; ++y) {
+            for (float x = 0; x < W; ++x) {
+                tileShape.setPosition({x * TILE, y * TILE});
+                // Empty tiles are white, wall tiles are red
+                if (grid.nodes[y][x].wall)
+                    tileShape.setFillColor(sf::Color::Red);
+                else
+                    tileShape.setFillColor(sf::Color::White);
+                window.draw(tileShape);
+            }
+        }
+
+        // Display screen
         window.display();
     }
-
     return 0;
 }
